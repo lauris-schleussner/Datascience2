de5eefe8ae1368378705bd54c6a9284747ba1988
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2022-10-03 01:48:58 +0200

    reordered some files

--- example.py
+++ example.py
@@ -13,26 +13,8 @@ import json
 from tqdm import tqdm
 import time
 
+from utils import load_stops, load_stop_times
 
-# load all stops from stops.txt
-def load_stops():
-    stops = []
-    with open("gtfsmdvlvb/stops.txt") as f:
-        txt_data = f.read()
-    for elem in txt_data.split("\n")[1:]:
-        stops.append(elem.split(","))
-    stops.remove([''])
-    return stops
-
-# load all stop times from stop_times
-def load_stop_times():
-    stop_times = []
-    with open("gtfsmdvlvb/stop_times.txt") as f:
-        txt_data = f.read()
-    for elem in txt_data.split("\n")[1:]:
-        stop_times.append(elem.split(","))
-    stop_times.remove([''])
-    return stop_times
 
 # find all occurences of a stop id in stop_times-data
 def find_occ_numbers(stop_id, **kwargs):
---
+++ utils.py
@@ -0,0 +1,24 @@
+# load all stops from stops.txt
+def load_stops():
+    stops = []
+    with open("gtfsmdvlvb/stops.txt") as f:
+        txt_data = f.read()
+    for elem in txt_data.split("\n")[1:]:
+        stops.append(elem.split(","))
+    stops.remove([''])
+    return stops
+
+# load all stop times from stop_times
+def load_stop_times():
+    stop_times = []
+    with open("gtfsmdvlvb/stop_times.txt") as f:
+        txt_data = f.read()
+    for elem in txt_data.split("\n")[1:]:
+        stop_times.append(elem.split(","))
+    stop_times.remove([''])
+    return stop_times
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
